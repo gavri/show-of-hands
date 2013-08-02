d015fcfae0cd1134ced850ca7114a325f6b7f63c
Gavri Fernandez <gavri.fernandez@gmail.com>
2013-08-02 17:14:45 +0530

    Get winner

--- src/show_of_hands/core.clj
+++ src/show_of_hands/core.clj
@@ -5,4 +5,6 @@
   [& args]
   (def ballotSheets [[1 2 3 ] [2 3 1] [2 1 3]])
   (def firstChoices (map first ballotSheets))
+  (def freq (frequencies firstChoices))
+  (def winner (key (apply max-key val freq)))
 )
