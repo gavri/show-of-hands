68e8fefc1aa96cc628025d0a199afa73ecacecba
Gavri Fernandez <gavri.fernandez@gmail.com>
2013-08-11 20:00:18 +0530

    Removed random test data generation. Overkill

--- test/show_of_hands/plurality_test.clj
+++ test/show_of_hands/plurality_test.clj
@@ -1,18 +1,11 @@
-(ns show-of-hands.plurality-test (:require [show-of-hands.plurality :as plurality] [show-of-hands.test-data-factory :as factory] [midje.sweet :refer :all]))
+(ns show-of-hands.plurality-test (:require [show-of-hands.plurality :as plurality] [midje.sweet :refer :all]))
 
 (facts "plurality"
-       (fact "degenerate case"
-             (doseq [i (factory/random-small-integers 100)]
-               (plurality/winners [[i]]) => [i]
-             )
-             )
-       (fact "majority is plurality"
-             (plurality/winners [[1] [2] [2]]) => [2]
-            )
-       (fact "plurality need not be a majority"
-             (plurality/winners [[1] [1] [2] [2] [2] [3] [3]]) => [2]
-             )
+       (fact "degenerate case" (plurality/winners [[1]]) => [1])
+       (fact "majority is plurality" (plurality/winners [[1] [2] [2]]) => [2])
+       (fact "plurality need not be a majority" (plurality/winners [[1] [1] [2] [2] [2] [3] [3]]) => [2])
        (fact "plurality with equal winners"
              (plurality/winners [[1] [2]]) => [1 2]
+             (plurality/winners [[1] [2] [1] [2]]) => [1 2]
              )
        )
--- test/show_of_hands/test_data_factory.clj
+++
@@ -1,9 +0,0 @@
-(ns show-of-hands.test-data-factory)
-
-(defn random-integers-between [start end]
-  (repeatedly #(rand-nth (range start end)))
-  )
-
-(defn random-small-integers [n]
-  (take n (random-integers-between -1000 1000))
-)
